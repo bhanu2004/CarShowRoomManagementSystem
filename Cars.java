268c7c840adb3c25f59ff227deb5b80445d671b1
bhanu2004 <75109116+bhanu2004@users.noreply.github.com>
2023-10-10 17:52:59 +0530

    Update Cars.java

--- Cars.java
+++ Cars.java
@@ -1,41 +1,40 @@
 import java.util.Scanner;
 
-public class Cars extends Showroom implements utility{
-    String car_name;
-    String car_color;
-    String car_fuel_type;
-    int car_price;
-    String car_type;
-    String car_transmission;
+public class Cars {
+    private String car_name;
+    private String car_color;
+    private String car_fuel_type;
+    private int car_price;
+    private String car_type;
+    private String car_transmission;
 
-    @Override
-    public void get_details(){
-        System.out.println("NAME: "+car_name);
-        System.out.println("COLOR: "+car_color);
-        System.out.println("FUEL TYPE: "+car_fuel_type);
-        System.out.println("PRICE: "+car_price);
-        System.out.println("CAR TYPE: "+car_type);
-        System.out.println("TRANSMISSION: "+car_transmission);
+    public String getCarName() {
+        return car_name;
     }
 
-    @Override
-    public void set_details(){
-        Scanner sc = new Scanner(System.in);
+    public void getDetails() {
+        System.out.println("Name: " + car_name);
+        System.out.println("Color: " + car_color);
+        System.out.println("Fuel Type: " + car_fuel_type);
+        System.out.println("Price: " + car_price);
+        System.out.println("Car Type: " + car_type);
+        System.out.println("Transmission: " + car_transmission);
+    }
+
+    public void setDetails(Scanner sc) {
         System.out.println("======================= *** ENTER CAR DETAILS *** =======================");
-        System.out.println();
-        System.out.print("CAR NAME: ");
+        System.out.print("Car Name: ");
         car_name = sc.nextLine();
-        System.out.print(("CAR COLOR: "));
+        System.out.print("Car Color: ");
         car_color = sc.nextLine();
-        System.out.print("CAR FUEL TYPE(PETROL/DIESEL): ");
+        System.out.print("Car Fuel Type (PETROL/DIESEL/EV): ");
         car_fuel_type = sc.nextLine();
-        System.out.print("CAR PRICE: ");
+        System.out.print("Car Price: ");
         car_price = sc.nextInt();
         sc.nextLine();
-        System.out.print("CAR TYPE(SEDAN/SUV/HATCHBACK): ");
+        System.out.print("Car Type (SEDAN/SUV/HATCHBACK/SPORTS): ");
         car_type = sc.nextLine();
-        System.out.print("TRANSMISSION TYPE(AUTOMATIC/MANUAL): ");
+        System.out.print("Transmission Type (AUTOMATIC/MANUAL): ");
         car_transmission = sc.nextLine();
-        total_cars_in_stock++;
     }
 }
