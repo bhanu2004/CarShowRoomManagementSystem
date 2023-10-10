0d46be781fd7fa356cf169bb1415ca0b493aeb0b
bhanu2004 <75109116+bhanu2004@users.noreply.github.com>
2023-10-10 17:54:13 +0530

    Update Showroom.java

--- Showroom.java
+++ Showroom.java
@@ -1,36 +1,38 @@
-import java.sql.SQLOutput;
 import java.util.Scanner;
-public class Showroom implements utility{
-    String showroom_name;
-    String showroom_address;
-    int total_employees;
-    int total_cars_in_stock = 0;
-    String manager_name;
 
-    @Override
-    public void get_details() {
-        System.out.println("Showroom Name: "+showroom_name);
-        System.out.println("Showroom Address: "+showroom_address);
-        System.out.println("Manager Name: "+manager_name);
-        System.out.println("Total Employees: "+total_employees);
-        System.out.println("Total Cars In Stock: "+total_cars_in_stock);
+public class Showroom {
+    private String showroom_name;
+    private String showroom_address;
+    private int total_employees;
+    private int total_cars_in_stock = 0;
+    private String manager_name;
 
+    public String getShowroomName() {
+        return showroom_name;
     }
-    @Override
-    public void set_details(){
-        Scanner sc = new Scanner(System.in);
-        System.out.println("======================= *** ENTER SHOWROOM DETAILS *** =======================");
-        System.out.println();
-        System.out.print("SHOWROOM NAME: ");
-        showroom_name = sc.nextLine();
-        System.out.print(("SHOWROOM ADDRESS: "));
-        showroom_address = sc.nextLine();
-        System.out.print("MANAGER NAME: ");
-        manager_name = sc.nextLine();
-        System.out.print("TOTAL NO OF EMPLOYEES: ");
-        total_employees = sc.nextInt();
-        System.out.print("TOTAL CARS IN STOCK: ");
-        total_cars_in_stock = sc.nextInt();
 
+    public void getDetails() {
+        System.out.println("Showroom Name: " + showroom_name);
+        System.out.println("Showroom Address: " + showroom_address);
+        System.out.println("Manager Name: " + manager_name);
+        System.out.println("Total Employees: " + total_employees);
+        System.out.println("Total Cars In Stock: " + total_cars_in_stock);
+    }
+
+    public void setDetails(Scanner sc) {
+        System.out.println("======================= *** ENTER SHOWROOM DETAILS *** =======================");
+        System.out.print("Showroom Name: ");
+        showroom_name = sc.nextLine();
+        System.out.print("Showroom Address: ");
+        showroom_address = sc.nextLine();
+        System.out.print("Manager Name: ");
+        manager_name = sc.nextLine();
+        System.out.print("Total Employees: ");
+        total_employees = sc.nextInt();
+        sc.nextLine();
+    }
+
+    public int getTotalCarsInStock() {
+        return total_cars_in_stock;
     }
 }
